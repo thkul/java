f7a454ebab9773448de17287498e5d8c6d42167a
imtable <mrelbat@gmail.com>
2025-12-09 23:38:46 +0000

    state pattern

---
+++ patterns/gof_behavioral/state.java
@@ -0,0 +1,41 @@
+package patterns.gof_behavioral;
+
+public class state {
+  interface OrderState {  // State interface
+    void pay(Order ctx); void ship(Order ctx);
+  }
+
+  class Order { // Context
+    private OrderState state = new NewState();
+    void setState(OrderState s) { state = s; }
+    void pay() { state.pay(this); }
+    void ship() { state.ship(this); }
+  }
+
+  class NewState implements OrderState { // States
+    public void pay(Order ctx) {
+      System.out.println("Paid.");
+      ctx.setState(new PaidState());
+    }
+    public void ship(Order ctx) {
+      System.out.println("Cannot ship: not paid.");
+    }
+  }
+
+  class PaidState implements OrderState {
+    public void pay(Order ctx) { System.out.println("Already paid."); }
+    public void ship(Order ctx) {
+      System.out.println("Shipped.");
+      ctx.setState(new ShippedState());
+    }
+  }
+
+  class ShippedState implements OrderState {
+    public void pay(Order ctx) {
+      System.out.println("Cannot pay: shipped.");
+    }
+    public void ship(Order ctx) {
+      System.out.println("Already shipped.");
+    }
+  }
+}
